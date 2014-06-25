aab8ac697613116077f7262b63fa877ebee2e778
xarvh <francesco.orsenigo@adslot.com>
2014-06-25 20:03:01 +1000

    correctly calculate hash

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
 
 import base64
+import hashlib
 
 def websocketHandshake(raw_request):
 
     request = dict([line.split(': ') for line in raw_request.split('\r\n')[1:] if line])
 
-    key = base64.b64decode(request['Sec-WebSocket-Key'])
-    response_key = base64.b64encode(key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
+    magic = request['Sec-WebSocket-Key'].strip() + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+    key = base64.b64encode(hashlib.sha1(magic).digest())
 
     response = '\r\n'.join([
         'HTTP/1.1 101 Switching Protocols',
         'Upgrade: websocket',
         'Connection: Upgrade',
-        'Sec-WebSocket-Accept: ' + response_key,
+        'Sec-WebSocket-Accept: ' + key,
         'Sec-WebSocket-Protocol: chat',
         ''
         ''
@@ -50,6 +51,7 @@ while True:
     log.write(data)
     answer = websocketHandshake(data)
     if answer:
+        print 'sending', answer
         sock.send(answer)
         while True:
             data = sock.recv(1024)
